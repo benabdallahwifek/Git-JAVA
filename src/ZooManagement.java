54617620806b70276217621f2b83dda403f22b04
wifek <benabdallah.wifek@esprit.tn>
2023-12-10 17:28:29 +0100

    Prosit1 Done

---
+++ src/ZooManagement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages = 20;
+    String zooName = "myZoo";
+
+    public static void main(String[] args) {
+        ZooManagement zm = new ZooManagement();
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisir le nombre de cages:");
+        int nbrCagesSaisie = sc.nextInt();
+        ZooManagement zm1 = new ZooManagement();
+        zm1.nbrCages = nbrCagesSaisie;
+    }
+}
